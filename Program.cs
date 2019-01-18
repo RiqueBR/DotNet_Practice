cedfe9547b193dfe0501437385326a144d71e87a
Henrique Pereira Batista <h.p.batista20@gmail.com>
2019-01-18 10:04:10 +0000

    initial practice on .net

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet__practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Rique!");    
+        }
+    }
+}
